c195492aedd80ac839853e9d7aea5596276b9db1
Freshcoder335 <pow21016@byui.edu>
2023-01-05 15:12:20 -0800

    Added famous bond greeting

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string JP1name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string JP2name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {JP2name}, {JP1name} {JP2name}. ");
     }
 }
