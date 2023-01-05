7268ebfe677cdf1e7422f21eb4589c5d88ccc6cf
Freshcoder335 <pow21016@byui.edu>
2023-01-05 15:47:45 -0800

    Completed all core requirements only but completed the nonetheless

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("What is your grade Percent? ");
+        string JPresponse = Console.ReadLine();
+        int JPgrade = int.Parse(JPresponse);
+
+        string JPvalue = "";
+
+        if (JPgrade >= 90)
+        {
+           JPvalue = "A";
+        }
+        else if (JPgrade >= 80)
+        {
+            JPvalue = "B";
+        }
+        else if (JPgrade >= 70)
+        {
+            JPvalue = "C";
+        }
+        else if (JPgrade >= 60)
+        {
+            JPvalue = "D";
+        }
+        else
+        {
+            JPvalue = "F";
+        }
+
+        Console.WriteLine($"Your Class Grade is: {JPvalue}");
+        if (JPgrade >= 70)
+        {
+            Console.WriteLine("You have passed this class. Congragulations!");
+        }
+        else
+        {
+            Console.WriteLine("You can always retake the course. ");
+        }
     }
 }
